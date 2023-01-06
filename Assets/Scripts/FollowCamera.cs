87096d4f57db8be2570906138016f0782944a185
Remi Tobias <remitobias1@outlook.com>
2023-01-06 01:35:19 +0000

    Basic Movement Tweaks

--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -4,7 +4,7 @@ public class FollowCamera : MonoBehaviour
 {
     [SerializeField] private Transform target;
 
-    private void Update()
+    private void LateUpdate()
     {
         transform.position = target.position;
     }

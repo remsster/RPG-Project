ca454e7b52cc88e25240e2e60e36c8a6b0c5f6b6
Remi Tobias <remitobias1@outlook.com>
2023-01-10 14:08:05 +0000

    Implementing Action Priority

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -12,15 +12,16 @@ namespace RPG.Control
         // ---------------------------------------------------------------------------------
         private void Update()
         {
-            InteractWithCombat();
-            InteractWithMovement();
+            if (InteractWithCombat()) return;
+            if (InteractWithMovement()) return;
+            print("nothing to do");
         }
 
         // ---------------------------------------------------------------------------------
         // Custom Methods
         // ---------------------------------------------------------------------------------
 
-        private void InteractWithCombat()
+        private bool InteractWithCombat()
         {
             RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
             foreach (RaycastHit hit in hits)
@@ -32,26 +33,37 @@ namespace RPG.Control
                 {
                     GetComponent<Fighter>().Attack(target);
                 }
+                return true;
             }
+            // We didnt find any combat targets to interact with
+            return false;
         }
 
-        private void InteractWithMovement()
-        {
-            if (Input.GetMouseButton(0))
-            {
-                MoveToCursor();
-            }
-        }
+        //private bool InteractWithMovement()
+        //{
+        //    if (Input.GetMouseButton(0))
+        //    {
+        //        MoveToCursor();
+        //        return true;
+        //    }
+        //    return false;
+        //}
 
-        private void MoveToCursor()
+        private bool InteractWithMovement()
         {
             Ray ray = GetMouseRay();
             RaycastHit hit;
             bool hasHit = Physics.Raycast(ray, out hit);
             if (hasHit)
             {
-                GetComponent<Mover>().MoveTo(hit.point);
+
+                if (Input.GetMouseButton(0))
+                {
+                    GetComponent<Mover>().MoveTo(hit.point);
+                }
+                return true;
             }
+            return false;
         }
 
         private static Ray GetMouseRay()

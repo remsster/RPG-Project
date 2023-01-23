2768320f420c5c232c04c24ca3a77d24c6abb9fd
Remi Tobias <remitobias1@outlook.com>
2023-01-23 14:23:19 +0000

    Scene Loading Portals

---
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace RPG.SceneManagement
+{
+    public class Portal : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.tag == "Player")
+            {
+                SceneManager.LoadScene(1);
+            }
+        }
+    }
+}

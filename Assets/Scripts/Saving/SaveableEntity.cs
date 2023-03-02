52f095b66601a67128ba50deb6637c89bf627821
Remi Tobias <remitobias1@outlook.com>
2023-03-02 16:57:14 +0000

    Saveable Entities (Again)

---
+++ Assets/Scripts/Saving/SaveableEntity.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace RPG.Saving
+{
+    public class SaveableEntity : MonoBehaviour
+    {
+        public string GetUniqueIdentifier() => "";
+
+        public object CaptureState()
+        {
+            print("Capturing state for " + GetUniqueIdentifier());
+            return null;
+        }
+
+        public void RestoreState(object state)
+        {
+            print("Restorign state for " + GetUniqueIdentifier());
+        }
+    }
+}
--- Assets/Scripts/Saving/SavingSystem.cs
+++ Assets/Scripts/Saving/SavingSystem.cs
@@ -1,9 +1,9 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
-using System.Text;
 using UnityEngine;
-using UnityEngine.AI;
+
 
 namespace RPG.Saving
 {
@@ -21,27 +21,27 @@ namespace RPG.Saving
             return Path.Combine(Application.persistentDataPath, saveFile);
         }
 
-        private byte[] SerializeVector(Vector3 vector)
+        private object CaptureState()
         {
-            byte[] vectorBytes = new byte[3 * 4];
-            BitConverter.GetBytes(vector.x).CopyTo(vectorBytes, 0);
-            BitConverter.GetBytes(vector.y).CopyTo(vectorBytes, 4);
-            BitConverter.GetBytes(vector.z).CopyTo(vectorBytes, 8);
-            return vectorBytes;
+            Dictionary<string, object> state = new Dictionary<string, object>();
+            int x = 0;
+            foreach(SaveableEntity saveable in FindObjectsOfType<SaveableEntity>())
+            {
+                state[saveable.GetUniqueIdentifier()] =  saveable.CaptureState();
+                x++;
+            }
+            print("Captured " + x + " states");
+            return state; 
         }
 
-        private Vector3 DeserializeVector3(byte[] buffer)
+        private void RestoreState(object state)
         {
-            Vector3 result = new Vector3();
-            result.x = BitConverter.ToSingle(buffer, 0);
-            result.y = BitConverter.ToSingle(buffer, 4);
-            result.z = BitConverter.ToSingle(buffer, 8);
-            return result;
-        }
+            Dictionary<string, object> stateDict = (Dictionary<string, object>)state;
+            foreach (SaveableEntity saveable in FindObjectsOfType<SaveableEntity>())
+            {
+                saveable.RestoreState(stateDict[saveable.GetUniqueIdentifier()]);
+            }
 
-        private Transform GetPlayerTransform()
-        {
-            return GameObject.FindWithTag("Player").transform;
         }
 
         // ---- Public ----
@@ -63,11 +63,8 @@ namespace RPG.Saving
 
             using(FileStream fstream = File.Open(path, FileMode.Create))
             {
-                Transform playerTransform = GetPlayerTransform();
-
                 BinaryFormatter formatter = new BinaryFormatter();
-                SerializableVector3 position = new SerializableVector3(playerTransform.position);
-                formatter.Serialize(fstream,  position);                
+                formatter.Serialize(fstream,  CaptureState());                
             }
         }
 
@@ -77,12 +74,12 @@ namespace RPG.Saving
 
             using (FileStream fstream = File.Open(path, FileMode.Open))
             {
-                Transform playerTransform = GetPlayerTransform();
                 
                 BinaryFormatter formatter = new BinaryFormatter();
-                playerTransform.position = ((SerializableVector3) formatter.Deserialize(fstream)).ToVector();
+                RestoreState(formatter.Deserialize(fstream));
+                
             }
-        }
+        }        
     }
 
 }

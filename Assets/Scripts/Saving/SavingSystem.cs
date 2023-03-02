7ed53a5e62c104f64cbf1d213e8510b85fa21b24
Remi Tobias <remitobias1@outlook.com>
2023-03-02 08:07:18 +0000

    Understanding Unicode (Again)

--- Assets/Scripts/Saving/SavingSystem.cs
+++ Assets/Scripts/Saving/SavingSystem.cs
@@ -1,4 +1,5 @@
 using System.IO;
+using System.Text;
 using UnityEngine;
 
 
@@ -15,7 +16,16 @@ namespace RPG.Saving
 
         public void Save(string saveFile)
         {
-            print("Saving to " + GetPathFromSaveFile(saveFile));
+            string path = GetPathFromSaveFile(saveFile);
+            print("Saving to " + path);
+            FileStream fstream = File.Open(path, FileMode.Create);
+
+            string text = "Hello World";
+            byte[] buffer = Encoding.UTF8.GetBytes(text);
+            byte[] spanishBuffer = { 0x0d, 0xc2 ,0xa1, 0x48, 0x6f, 0x6c, 0x61, 0x20, 0x4d, 0x75, 0x6e, 0x64, 0x6f, 0x21 };
+            fstream.Write(buffer, 0, buffer.Length);
+            fstream.Write(spanishBuffer, 0, spanishBuffer.Length);
+            fstream.Close();
         }
 
         public void Load(string saveFile)

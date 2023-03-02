79c033c2a721c8c7129e65c6eeb393e78b20cdf5
Remi Tobias <remitobias1@outlook.com>
2023-03-02 10:14:04 +0000

    BinaryFormatter For Serialization (Again)

--- Assets/Scripts/Saving/SavingSystem.cs
+++ Assets/Scripts/Saving/SavingSystem.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using UnityEngine;
 using UnityEngine.AI;
@@ -63,8 +64,10 @@ namespace RPG.Saving
             using(FileStream fstream = File.Open(path, FileMode.Create))
             {
                 Transform playerTransform = GetPlayerTransform();
-                byte[] buffer = SerializeVector(playerTransform.position);
-                fstream.Write(buffer, 0, buffer.Length);
+
+                BinaryFormatter formatter = new BinaryFormatter();
+                SerializableVector3 position = new SerializableVector3(playerTransform.position);
+                formatter.Serialize(fstream,  position);                
             }
         }
 
@@ -74,10 +77,10 @@ namespace RPG.Saving
 
             using (FileStream fstream = File.Open(path, FileMode.Open))
             {
-                byte[] buffer = new byte[fstream.Length];
-                fstream.Read(buffer, 0, buffer.Length);
                 Transform playerTransform = GetPlayerTransform();
-                playerTransform.position = DeserializeVector3(buffer);
+                
+                BinaryFormatter formatter = new BinaryFormatter();
+                playerTransform.position = ((SerializableVector3) formatter.Deserialize(fstream)).ToVector();
             }
         }
     }

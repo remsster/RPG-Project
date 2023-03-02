6b2a7df7c7edad27be8712b005ee1277290b15ac
Remi Tobias <remitobias1@outlook.com>
2023-03-02 19:46:52 +0000

    Editing Serialized Fields (Again)

--- Assets/Scripts/Saving/SaveableEntity.cs
+++ Assets/Scripts/Saving/SaveableEntity.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using UnityEditor;
+using UnityEngine;
 
 namespace RPG.Saving
 {
@@ -7,13 +8,32 @@ namespace RPG.Saving
     {
         [SerializeField] string uniqueIdentifier = "";
 
-        public string GetUniqueIdentifier() => "";
+        public string GetUniqueIdentifier() => uniqueIdentifier;
 
+        // ---------------------------------------------------------------------------------
+        // Unity Engine Methods
+        // ---------------------------------------------------------------------------------
+#if UNITY_EDITOR
+        // Not included in build
         private void Update()
         {
             if (Application.IsPlaying(gameObject)) return;
-            print("Editing");
+            if (string.IsNullOrEmpty(gameObject.scene.path)) return;
+            SerializedObject serializedObject = new SerializedObject(this);
+            SerializedProperty property = serializedObject.FindProperty(nameof(uniqueIdentifier));
+            
+            if (string.IsNullOrEmpty(property.stringValue))
+            {
+                property.stringValue = System.Guid.NewGuid().ToString();
+                serializedObject.ApplyModifiedProperties();
+            }
+
         }
+#endif
+
+        // ---------------------------------------------------------------------------------
+        // Custom Methods
+        // ---------------------------------------------------------------------------------
 
         public object CaptureState()
         {

8fa215898e86ae3cf096384ddf3f2e9e35588925
Remi Tobias <remitobias1@outlook.com>
2023-03-02 08:13:26 +0000

    FileStream Read And Write (Again)

--- Assets/Scripts/Saving/SavingSystem.cs
+++ Assets/Scripts/Saving/SavingSystem.cs
@@ -18,19 +18,29 @@ namespace RPG.Saving
         {
             string path = GetPathFromSaveFile(saveFile);
             print("Saving to " + path);
-            FileStream fstream = File.Open(path, FileMode.Create);
+            ;
 
-            string text = "Hello World";
-            byte[] buffer = Encoding.UTF8.GetBytes(text);
-            byte[] spanishBuffer = { 0x0d, 0xc2 ,0xa1, 0x48, 0x6f, 0x6c, 0x61, 0x20, 0x4d, 0x75, 0x6e, 0x64, 0x6f, 0x21 };
-            fstream.Write(buffer, 0, buffer.Length);
-            fstream.Write(spanishBuffer, 0, spanishBuffer.Length);
-            fstream.Close();
+            using(FileStream fstream = File.Open(path, FileMode.Create))
+            {
+                string text = "Hello World";
+                byte[] buffer = Encoding.UTF8.GetBytes(text);
+                byte[] spanishBuffer = { 0x0d, 0xc2 ,0xa1, 0x48, 0x6f, 0x6c, 0x61, 0x20, 0x4d, 0x75, 0x6e, 0x64, 0x6f, 0x21 };
+                fstream.Write(buffer, 0, buffer.Length);
+                fstream.Write(spanishBuffer, 0, spanishBuffer.Length);
+            }
         }
 
         public void Load(string saveFile)
         {
-            print("Loading from " + GetPathFromSaveFile(saveFile));
+            string path =  GetPathFromSaveFile(saveFile);
+
+            using (FileStream fstream = File.Open(path, FileMode.Open))
+            {
+                byte[] buffer = new byte[fstream.Length];
+                fstream.Read(buffer,0, buffer.Length);
+                string text = Encoding.UTF8.GetString(buffer);
+                Debug.Log(text);
+            }
         }
     }
 
